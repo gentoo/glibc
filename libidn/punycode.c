4f3552fa72f83b77b757886ef66e60fd48a90fc9
Tim Rühsen <tim.ruehsen@gmx.de>
2017-10-04 13:22:43 +0200

    libidn/punycode.c (decode_digit): Really fix integer overflow
    libidn/punycode.c (decode_digit): Really fix integer overflow
    
    The fix in commit e9e81b8063b095b02cf104bb992fa9bf9515b9d8
    was incomplete.
    
    Reported-by: Christian Weisgerber

--- libidn/punycode.c
+++ libidn/punycode.c
@@ -81,8 +81,8 @@ enum
 static unsigned
 decode_digit (int cp)
 {
-  return (unsigned) cp - 48 < 10 ? cp - 22 : cp - 65 < 26 ? cp - 65 :
-    cp - 97 < 26 ? cp - 97 : base;
+  return (unsigned) (cp - 48 < 10 ? cp - 22 : cp - 65 < 26 ? cp - 65 :
+    cp - 97 < 26 ? cp - 97 : base);
 }
 
 /* encode_digit(d,flag) returns the basic code point whose value      */
