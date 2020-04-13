8bdfb147aa546cc5693cf43658b4c7bf142e63fa
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2020-04-13 18:09:20 -0300

    linux: Set internal DIR filepos as off64_t [BZ #23960, BZ #24050]
    It allows to obtain the expected entry offset on telldir and set
    it correctly on seekdir on platforms where long int is smaller
    than off64_t.
    
    On such cases telldir will mantain an internal list that maps the
    DIR object off64_t offsets to the returned long int (the function
    return value).  The seekdir will then set the correct offset from
    the internal list using the telldir as the list key.
    
    It also removes the overflow check on readdir and the returned value
    will be truncated by the non-LFS off_t size.  As Joseph has noted
    in BZ #23960 comment #22, d_off is an opaque value and since
    telldir/seekdir works regardless of the returned dirent d_off value.
    
    Finally it removed the requirement to check for overflow values on
    telldir (BZ #24050).
    
    Checked on x86_64-linux-gnu, i686-linux-gnu, powerpc-linux-gnu,
    and arm-linux-gnueabihf.

--- sysdeps/unix/sysv/linux/dirstream.h
+++ sysdeps/unix/sysv/linux/dirstream.h
@@ -21,6 +21,7 @@
 #include <sys/types.h>
 
 #include <libc-lock.h>
+#include <telldir.h>
 
 /* Directory stream type.
 
@@ -37,13 +38,16 @@ struct __dirstream
     size_t size;		/* Total valid data in the block.  */
     size_t offset;		/* Current offset into the block.  */
 
-    off_t filepos;		/* Position of next entry to read.  */
+    off64_t filepos;		/* Position of next entry to read.  */
 
     int errcode;		/* Delayed error code.  */
 
 #if !defined __OFF_T_MATCHES_OFF64_T || !defined __INO_T_MATCHES_INO64_T
     char *tbuffer;		/* Translation buffer for non-LFS calls.  */
     size_t tbuffer_size;	/* Size of translation buffer.  */
+#endif
+#ifndef __LP64__
+    struct dirstream_loc_t locs; /* off64_t to long int map for telldir.  */
 #endif
 
     /* Directory block.  We must make sure that this block starts
--- sysdeps/unix/sysv/linux/opendir.c
+++ sysdeps/unix/sysv/linux/opendir.c
@@ -150,6 +150,9 @@ __alloc_dir (int fd, bool close_fd, int flags,
   dirp->offset = 0;
   dirp->filepos = 0;
   dirp->errcode = 0;
+#ifndef __LP64__
+  dirstream_loc_init (&dirp->locs);
+#endif
 
   return dirp;
 }
